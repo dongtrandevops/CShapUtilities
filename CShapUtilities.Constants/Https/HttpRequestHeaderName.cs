4468dd0827c182625f95c45e833d8a53350b260f
DESKTOP-D7K5720\DongTranDevOps <dongtrandevops@gmail.com>
2022-02-26 10:33:12 +0700

    Initialize project

---
+++ CShapUtilities.Constants/Https/HttpRequestHeaderName.cs
@@ -0,0 +1,11 @@
+﻿namespace CShapUtilities.Constants.Https
+{
+    public static class HttpRequestHeaderName
+    {
+        public const string Accept = "accept";
+        public const string AcceptEncoding = "accept-encoding";
+        public const string AcceptLanguage = "accept-language:";
+        public const string UserAgent = "user-agent";
+        public const string Cookie = "cookie";
+    }
+}
---
+++ CShapUtilities.Constants/Https/HttpRequestHeaderValue.cs
@@ -0,0 +1,10 @@
+﻿namespace CShapUtilities.Constants.Https
+{
+    public static class HttpRequestHeaderValue
+    {
+        public const string Accept = "*/*";
+        public const string AcceptEncoding = "gzip, deflate, br";
+        public const string AcceptLanguage = "en,vi-VN;q=0.9,vi;q=0.8,fr-FR;q=0.7,fr;q=0.6,en-US;q=0.5";
+        public const string UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36";
+    }
+}
